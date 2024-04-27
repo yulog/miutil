2a19fa5285de70fb9271c2d5026e3fe723f98e66
yulog <29723448+yulog@users.noreply.github.com>
2024-04-27 21:53:14 +0900

    fix: use stream

--- fetch.go
+++ fetch.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"io"
 	"net/http"
+
+	"github.com/goccy/go-json"
 )
 
 // Post は指定のurlにJSONをPOSTする
@@ -25,4 +27,30 @@ func Post(url string, b []byte) ([]byte, error) {
 		return nil, err
 	}
 	return body, nil
+}
+
+// Post は指定のurlにJSONをPOSTし、ResponseのJSONを受け取る
+func Post2[T any](url string, b io.Reader) (*T, error) {
+	req, err := http.NewRequest(http.MethodPost, url, b)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json") // ないと Unsupported Media Type
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var out T
+	err = decodeBody(resp, &out)
+	if err != nil {
+		return nil, err
+	}
+	return &out, nil
+}
+
+func decodeBody(resp *http.Response, out any) error {
+	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(out)
 }
